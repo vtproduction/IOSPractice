6726a80fd066612ae74d0763b881a8df895819a8
NIEN-IMAC-TOMO <nienle@tomoapp.vn>
2018-01-18 19:00:44 +0700

    complete chap 1

---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by cityme on 1/18/18.
+//  Copyright © 2018 midsummer. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
